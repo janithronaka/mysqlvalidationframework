54392437fb911cf01069266a356ea600569c9c5f
Janith Ronaka <janithronaka@gmail.com>
2018-12-18 23:43:05 +0530

    version 1.0.5

---
+++ Server Code Genarator(June27)/Server Code Genarator/DBConnect.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Server_Code_Genarator
+{
+    public class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        public string uid;
+        public string password;
+
+
+        public DBConnect(String user, String pass)
+        {
+            uid = user;
+            password = pass;
+            Initialize();
+        }
+
+        public void SetDatabase(String DbName)
+        {
+            database = DbName;
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public string GetUsername()
+        {
+            return uid;
+        }
+
+        public string GetPassword()
+        {
+            return password;
+        }
+
+        //open connection to database
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        public String GetAllSchema()
+        {
+            String schema = "";
+            if(OpenConnection())
+                using (MySqlCommand cmd = new MySqlCommand("SELECT SCHEMA_NAME AS `Database` FROM INFORMATION_SCHEMA.SCHEMATA;", connection))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        while (reader.Read())
+                        {
+                           schema += reader[0].ToString() + "|";
+                        }
+                    }
+                }
+            CloseConnection();
+            return schema;
+        }
+
+        public String GetAllTables()
+        {
+            String schema = "";
+            if (OpenConnection())
+                using (MySqlCommand cmd = new MySqlCommand("SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = @dbname;", connection))
+                {
+                    cmd.Parameters.AddWithValue("@dbname",database);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        while (reader.Read())
+                        {
+                            schema += reader[0].ToString() + "|";
+                        }
+                    }
+                }
+            CloseConnection();
+            return schema;
+        }
+
+        public String GetAllAttributes(string table_)
+        {
+            string attrs = "";
+            if (OpenConnection())
+                using (MySqlCommand cmd = new MySqlCommand("SELECT column_name, data_type, character_maximum_length, is_nullable, column_key FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = @tablename;", connection))
+                {
+                    cmd.Parameters.AddWithValue("@tablename",table_);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        while (reader.Read())
+                        {
+                            string dfname = reader[0].ToString();
+                            string dftype = reader[1].ToString();
+                            string dfsize = reader[2].ToString();
+                            string dfmand = reader[3].ToString();
+                            string dfprim = reader[4].ToString();
+                            attrs += dfname + "^" + dftype + "^"+ dfsize + "^" + dfmand + "^" + dfprim + "|";
+                        }
+                    }
+                }
+            CloseConnection();
+            return attrs;
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ Server Code Genarator(June27)/Server Code Genarator/frmForeignKey.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Server_Code_Genarator
+{
+    partial class frmForeignKey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbTable = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tblRelation = new System.Windows.Forms.DataGridView();
+            this.colRef = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.collocal = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tblRelation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbTable
+            // 
+            this.cmbTable.FormattingEnabled = true;
+            this.cmbTable.Location = new System.Drawing.Point(109, 10);
+            this.cmbTable.Name = "cmbTable";
+            this.cmbTable.Size = new System.Drawing.Size(272, 21);
+            this.cmbTable.TabIndex = 0;
+            this.cmbTable.SelectedIndexChanged += new System.EventHandler(this.cmbTable_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Reference Table:";
+            // 
+            // tblRelation
+            // 
+            this.tblRelation.AllowUserToAddRows = false;
+            this.tblRelation.AllowUserToDeleteRows = false;
+            this.tblRelation.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tblRelation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblRelation.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colRef,
+            this.collocal});
+            this.tblRelation.Location = new System.Drawing.Point(13, 52);
+            this.tblRelation.Name = "tblRelation";
+            this.tblRelation.Size = new System.Drawing.Size(368, 150);
+            this.tblRelation.TabIndex = 2;
+            // 
+            // colRef
+            // 
+            this.colRef.HeaderText = "Reference";
+            this.colRef.Name = "colRef";
+            this.colRef.ReadOnly = true;
+            // 
+            // collocal
+            // 
+            this.collocal.HeaderText = "Local Column";
+            this.collocal.Name = "collocal";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmForeignKey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 236);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tblRelation);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbTable);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmForeignKey";
+            this.ShowInTaskbar = false;
+            this.Text = "Set Reference";
+            this.Load += new System.EventHandler(this.frmForeignKey_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblRelation)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView tblRelation;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRef;
+        private System.Windows.Forms.DataGridViewComboBoxColumn collocal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
